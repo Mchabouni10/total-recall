90c12a3deeea6f3dfd5beadd8069c54986879768
MOULOUD CHABOUNI <mouloud619@gmail.com>
2023-10-24 13:46:44 -0500

    4th try

--- totalrecall.js
+++ totalrecall.js
@@ -116,6 +116,8 @@
 //     console.log("You are too young")
 // };
 
+
+
 // ============
 // LOOPS
 // ============
@@ -135,9 +137,147 @@
 
 
 
+// ===============
+// GET EVEN
+// ===============
+// 1.Print out the numbers that are within the range of 1 - 100
+//  for(let i = 0; i <= 100; i++){
+//     console.log(i)   
+//  }
+//  2.Adjust your code to add a message next to even numbers only that says: "<-- is an even number"
+
+// for(let i = 0; i <= 100; i++){
+    
+//     if(i % 2 === 0){
+//         console.log(i+"<-- is an even number" )
+//     }  
+//     else{ 
+//         console.log(i)
+//     } 
+//  }
 
 
 
+// =================
+// 5. Give me Five
+// ===================
+//1.For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five
+// for(let i = 0; i <= 100; i++){
+//     if(i % 5 === 0){
+//         console.log('I found : ' + i + ". High five!" )
+//     }    
+// }
+// 1.For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five
+// for(let i = 0; i <= 100; i++){
+//     if(i % 5 === 0){
+//         console.log('I found : ' + i + ". High five!" )
+//     }
+//      else if(i % 3 === 0){
+//         console.log('I found : ' + i + ". three is a crowd")
+//      }   
+// }
+// 1.For numbers divisible by both three and five, be sure your code prints both messages
+// for(let i = 0; i <= 100; i++){
+//     if(i % 5 === 0 && i % 3 === 0){
+//         console.log(`I found : ${i} high five and three is a crowd`)
+//     }
+//      else if(i % 3 === 0){
+//         console.log('I found : ' + i + ". three is a crowd")
+//      }  
+//      else if( i % 5 === 0){
+//         console.log('I found : ' + i + ". High five!" )
+        
+
+//      } 
+// }
 
 
- 
+
+//======================================
+//D. Savings account
+//=====================================
+// 1. Write code that will save the sum of all the numbers between 1 - 10 
+//to a variable called bank_account.
+
+// let bank_account = 0 ;
+// for (let i = 1; i <= 10; i++ ){
+//     bank_account = i + bank_account 
+// }
+// console.log('my bank account:' + bank_account)
+
+// 2. You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all 
+//the numbers between 1 - 100 multiplied by 2.
+
+// let bank_account = 0 ;
+// for (let i = 1; i <= 100; i++ ){
+//     bank_account = (i * 2) + bank_account
+// }
+// console.log('my bank account:' + bank_account)
+
+
+ //=====================================
+ // III. Arrays & Control flow
+ //======================================
+
+//A. Talk about it:
+ // 1. What are the things in an array called?
+ // a elements or items 
+ // 2. Do Arrays guarantee those things will be in order?
+ // Yes always things inside an Array goes by order
+ //3. What real-life thing could you model with an array?
+ // fuits basket
+
+ //B. Easy Does It
+//  const quotes = ['banna','strawberries','orage']
+//  console.log(quotes[0])
+//  console.log(quotes[1])
+//  console.log(quotes[2])
+
+ //C. Accessing elements
+//  //1. How do you access the 1st element in the array?
+//  const randomThings = [1, 10, "Hello", true]
+//  console.log(randomThings[0])
+
+ //2. Change the value of "Hello"to "World"
+//  randomThings[2] = 'World'
+//  console.log(randomThings[2])
+
+ //3. Check the value of the array 
+ //to make sure it updated the array. How? Why, yes! console.log();
+//  console.log(randomThings[2])
+
+//D. Change values
+//1.What would you write to access the 3rd element of the array?
+// const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
+// console.log(ourClass[2])
+// //2. Change the value of "Github" to "Octocat"
+// ourClass[4] = 'Octocat'
+// console.log(ourClass)
+//3. Add a new element, "Cloud City" to the array
+// ourClass.push('Cloud City')
+// console.log(ourClass)
+
+//E. Mix It Up
+// const myArray = [5, 10, 500, 20]
+//1. Add the string "Aegon"to the end of the array. Add another string 
+//of your choice to the end of the array.
+// myArray.push('Aegon','Winter')
+// console.log(myArray)
+//2. Remove the 5from the beginning of the array
+// myArray.shift()
+// console.log(myArray)
+//3. Add the string "Bob Marley"to the beginning of the array.
+// myArray.unshift('Bob Marley')
+// console.log(myArray)
+//4. Remove the string of your choice from the end of the array.
+// myArray.pop()
+// console.log(myArray)
+//5. Reverse this array using Array.prototype.reverse(). Did you mutate the array?
+// What does mutate mean? Did the .reverse()method return anything?
+// myArray.reverse()
+// console.log(myArray)
+//What does mutate mean?
+//Mutate mean changing order in our original Array
+//Did the .reverse()method return anything?
+// Yes it changed the order and reverse the elements inside our Array
+
