534678e77d956bd4719fb86f2d1b27fe86467a69
MOULOUD CHABOUNI <mouloud619@gmail.com>
2023-10-24 14:39:09 -0500

    fourth try

--- totalrecall.js
+++ totalrecall.js
@@ -281,3 +281,94 @@
 //Did the .reverse()method return anything?
 // Yes it changed the order and reverse the elements inside our Array
 
+//===================================================
+// F. Biggie Smalls
+//===================================================
+// 1. Create a variable that contains an integer.
+// let variable = 150;
+
+// // Write an if ... elsestatement that:
+
+// if (variable < 100) {
+//     console.log('little number')
+// } else console.log('big number')
+
+// G. Monkey in the Middle
+// Write an if ... else if ... elsestatement:
+// let number = 8;
+
+// if (number < 5) {
+//     console.log('little number')
+// } else if (number > 10) {
+//     console.log('big number')
+// } else {
+//     console.log('monkey')
+// }
+
+// 2. console.log()little numberif the number entered is less than 5.
+// If the number entered is more than 10, log big number.
+// Otherwise, log "monkey".
+
+// H. What's in Your Closet?
+// Below, we've given you examples of Kristyn and 
+// Thom's closets modeled as data in JavaScript.
+
+// const kristynsCloset = [
+//     "left shoe",
+//     "cowboy boots",
+//     "right sock",
+//     "Per Scholas hoodie",
+//     "green pants",
+//     "yellow knit hat",
+//     "marshmallow peeps"
+// ];
+
+// // Thom's closet is more complicated. Check out this nested data structure!!
+// const thomsCloset = [
+//     [
+//         // These are Thom's shirts
+//         "grey button-up",
+//         "dark grey button-up",
+//         "light blue button-up",
+//         "blue button-up",
+//     ], [
+//         // These are Thom's pants
+//         "grey jeans",
+//         "jeans",
+//         "PJs"
+//     ], [
+//         // Thom's accessories
+//         "wool mittens",
+//         "wool scarf",
+//         "raybans"
+//     ]
+// ];
+// What's Kristyn wearing today? Using bracket notation to
+// access items in kristynsCloset, log the sentence
+// "Kristyn is rocking that " + the third item in Kristyn's closet
+// + " today!" to the console.
+// console.log(`Kristyn is rocking that ${kristynsCloset[2]} today!`)
+
+// Kristyn just bought some sweet shades! 
+// Add "raybans"to her closet after "yellow knit hat".
+// kristynsCloset.splice(6, 0, 'raybans')
+// console.log(kristynsCloset)
+// Kristyn spilled coffee on her hat... modify this 
+// item to read "stained knit hat"instead of yellow.
+// kristynsCloset[5] = 'stained knit hat'
+// console.log(kristynsCloset)
+// Put together an outfit for Thom! Using bracket notation, 
+// access the first element in Thom's shirtsarray.
+// console.log(thomsCloset[0][1])
+// In the same way, access one item from Thom's pants array.
+//console.log(thomsCloset[1][2])
+// Access one item from Thom's accessories array.
+//console.log(thomsCloset[2][1])
+// Log a sentence about what Thom's wearing. 
+// Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+//console.log(`Thom is looking fierce in a ${thomsCloset[0][0]}, ${thomsCloset[1][1]} and ${thomsCloset[2][1]}`)
+// Get more specific about what kind of PJs Thom's wearing this winter.
+// Modify the name of his PJ pants to Footie Pajamas.
+thomsCloset[1][2] = 'Footie Pajamas'
+// console.log(thomsCloset)
+
